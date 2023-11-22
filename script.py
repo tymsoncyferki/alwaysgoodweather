f61300bb6a868adab9895fa771af282f5b8c138c
tymsoncyferki <tymekurban@gmail.com>
2023-11-22 23:58:34 +0100

    more to know

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from plotnine import geom_point, ggplot, aes
 
 data = pd.read_csv('locations.csv')
-y = data['hmax'] # tylko do testów
+y = data['hmax']  # only for tests
 X = data[['latitude', 'longitude']]
 lm = LinearRegression().fit(X.values, y.values)
 bialystok = lm.predict(np.array([[53.175887, 19.745537]]))
+
+
 print(bialystok)
