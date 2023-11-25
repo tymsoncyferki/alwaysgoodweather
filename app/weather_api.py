d0f785ae814c7581b850026ce73a4d206bb54eb1
tymsoncyferki <tymekurban@gmail.com>
2023-11-25 18:22:51 +0100

    environment setup

--- app/weather_api.py
+++ app/weather_api.py
@@ -1,13 +1,17 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
 
 
 class WeatherApi:
-    api_key = "797dbfda9acd48d6abb182425232111"
+    api_key = os.environ.get("API_KEY")
 
     @staticmethod
     def get_response_history(location, date, key=api_key):
         response = requests.get(
             f"https://api.weatherapi.com/v1/history.json?key={key}&q={location}&dt={date}")
         return json.loads(response.text), response.status_code
-
