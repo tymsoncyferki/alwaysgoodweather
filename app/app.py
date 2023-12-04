e4efa14ee8cdefee29dbb7ff8ca32c7caf02a75d
tymsoncyferki <tymekurban@gmail.com>
2023-12-04 20:32:53 +0100

    render map

--- app/app.py
+++ app/app.py
@@ -1,17 +1,24 @@
 from shiny import App, render, ui
+from joblib import load
+from data.data import plot_map
 
 app_ui = ui.page_fluid(
     ui.panel_title("alwaysgoodweather.com"),
-    ui.input_slider("number", "Degrees", 0, 100, 20),
+    ui.input_slider("number", "Degrees", 0, 1000, 100),
     ui.output_text_verbatim("txt"),
+    ui.output_plot("plot")
 )
 
 
 def server(input, output, session):
-
     @render.text
     def txt():
         return f"waether is fucked {input.number() * 2} degrees"
+
+    @render.plot
+    def plot():
+        model = load('temp_model.joblib')
+        return plot_map(input.number(), 2, model, -input.number() + 550)
 
 
 app = App(app_ui, server)
