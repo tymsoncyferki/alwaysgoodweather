96272e2154f1c398a390cb8a5034d97caf0e2eb2
tymsoncyferki <tymekurban@gmail.com>
2023-12-04 23:22:28 +0100

    api calls and tests

--- app/weather_api.py
+++ app/weather_api.py
@@ -15,3 +15,16 @@ class WeatherApi:
         response = requests.get(
             f"https://api.weatherapi.com/v1/history.json?key={key}&q={location}&dt={date}")
         return json.loads(response.text), response.status_code
+
+    @staticmethod
+    def get_response_weather(location, key=api_key):
+        response = requests.get(
+            f"https://api.weatherapi.com/v1/current.json?key={key}&q={location}&aqi=no")
+        return json.loads(response.text), response.status_code
+
+    @staticmethod
+    def get_response_forecast(location, days, key=api_key):
+        assert days in [1, 2, 3]
+        response = requests.get(
+            f"https://api.weatherapi.com/v1/forecast.json?key={key}&q={location}&days={days}&aqi=no&alerts=no")
+        return json.loads(response.text), response.status_code
--- tests.py
+++ tests.py
@@ -12,10 +12,23 @@ class TestWeatherApi(unittest.TestCase):
         response, code = WeatherApi.get_response_history("piaseczno", date)
 
         self.assertEqual(code, 200)
-
+        self.assertEqual(response['location']['name'], "Piaseczno")
         self.assertEqual(response['location']['lat'], 52.08)
         self.assertEqual(response['location']['lon'], 21.03)
-        self.assertEqual(response['location']['name'], "Piaseczno")
+
+    def test_get_response_forecast(self):
+        response, code = WeatherApi.get_response_forecast("Warszawa", days=3)
+        today = datetime.now() + timedelta(days=2)
+        date = today.strftime('%Y-%m-%d')
+
+        self.assertEqual(code, 200)
+        self.assertEqual(response['forecast']['forecastday'][2]['date'], date)
+
+    def test_get_response_weather(self):
+        response, code = WeatherApi.get_response_weather("40,20")
+
+        self.assertEqual(code, 200)
+        self.assertEqual(response['location']['name'], 'Delvine')
 
 
 class TestData(unittest.TestCase):
