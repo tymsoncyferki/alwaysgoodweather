74a80ed6d78854f3b8ca63a9c987e72c2a9c6f21
tymsoncyferki <tymekurban@gmail.com>
2023-12-06 17:41:21 +0100

    first ui

--- app/app.py
+++ app/app.py
@@ -1,22 +1,49 @@
 from shiny import App, render, ui
 from joblib import load
 from data.data import plot_map
+from weather_api import WeatherApi
 
 app_ui = ui.page_fluid(
-    ui.panel_title("alwaysgoodweather.com"),
-    ui.input_slider("number", "Degrees", 0, 500, 100),
-    ui.output_text_verbatim("txt"),
-    ui.output_plot("plot")
+
+    ui.row(
+        ui.column(3),
+        ui.column(6,
+                  ui.panel_title("alwaysgoodweather.com"),
+                  ui.navset_tab(
+                      ui.nav("Weather",
+                             ui.input_text("location", "Enter location:"),
+                             ui.output_text_verbatim("current_temp"),
+                             ),
+                      ui.nav("About",
+                             ui.input_slider("number", "Degrees", 100, 500, 150),
+                             ui.output_plot("world_map"),
+                             ),
+                  ),
+                  ),
+        ui.column(3)
+    ),
+
 )
 
 
 def server(input, output, session):
+
     @render.text
-    def txt():
-        return f"weather is fucked {input.number() * 2} degrees"
+    def current_temp():
+        location = input.location()
+        try:
+            response, code = WeatherApi.get_response_weather(location)
+            print(response)
+            region = response['location']['region']
+            if region != "":
+                region = f'{region}, '
+            return f"Current weather in {response['location']['name']}, {region}" \
+                   f"{response['location']['country']} is {response['current']['temp_c']} Celsius degrees"
+        except KeyError:
+            return ""
 
     @render.plot
-    def plot():
+    def world_map():
         model = load('temp_model.joblib')
         return plot_map(input.number(), 2, model, -input.number() + 550)
 
